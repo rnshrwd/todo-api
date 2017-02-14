537ded9f3ae1e7e1ab9bb37531afa93924e1ee7c
Aaron Sherwood <rnshrwd@yahoo.com>
2017-02-14 11:28:53 -0800

    add ?completed filter

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+// 	name: 'Brillo',
+// 	age: '2'
+// };
+
+// function updatePerson (obj) {
+// 	// obj = {
+// 	// 	name: 'Brillo',
+// 	// 	age: '3'
+// 	// }
+
+// 	obj.age = 4;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+
+var array = [15, 37];
+
+function addGrade (array) {
+	array.push(50);
+
+	array = 71;
+}
+addGrade(array)
+console.log(array);
