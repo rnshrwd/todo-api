75c0d1cdd6d85a24a624f953221b2e8fe125dc25
Aaron Sherwood <rnshrwd@yahoo.com>
2017-04-17 18:06:55 -0700

    add login route and class method

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
+var bcryptjs = require('bcryptjs');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -129,27 +130,16 @@ app.post('/users', function (req, res) {
 //POST /users/login
 app.post('/users/login', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
-	if (typeof body.emil !== 'string' || typeof body.password !== 'string') {
-		return res.status(400).send();
-	}
 
-	db.user.findOne({
-		where: {
-			email: body.email
-		}
-	}).then(function (user) {
-		if(!user) {
-			return res.status(401).send();
-		}
-		res.json(user.toJSON());
-	}, function (e) {
-		res.status(500).send();
+	db.user.authenticate(body).then(function (user) {
+		res.json(user.toPublicJSON());
+	}, function () {
+		res.status(401).send();
 	});
 });
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force: true}).then(function () {
 	app.listen(PORT, function () {
 		console.log('Express listening on port ' + PORT + '!');
 	});
 });
-
