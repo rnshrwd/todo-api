da6313b42f1c4f6299185a42b20fcdcdd1705026
Aaron Sherwood <rnshrwd@yahoo.com>
2017-04-22 13:03:23 -0700

    add user file and remove eclipse project file

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ module.exports = function (sequelize, DataTypes) {
 				this.setDataValue('salt', salt);
 				this.setDataValue('password_hash', hashedPassword);
 			}
-		} 
+		}  
 	}, {
 		hooks: {
 			beforeValidate: function (user, options) {				
